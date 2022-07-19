037f2ea89819f90841b9984c1fd7b56d5b07ef9e
lucaskenp <lucasmonteirolima17@gmail.com>
2022-07-19 09:33:43 -0300

    API Rest School

---
+++ dist/database/seeds/20220718160223-criar-usuarios.js
@@ -0,0 +1,31 @@
+"use strict";const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  async up(queryInterface) {
+    return queryInterface.bulkInsert('users', [
+      {
+        nome: 'Lucas1',
+        email: 'lucas1@gmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Lucas2',
+        email: 'lucas2@gmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Lucas3',
+        email: 'lucas3@gmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ], {});
+  },
+
+  down() {},
+};
